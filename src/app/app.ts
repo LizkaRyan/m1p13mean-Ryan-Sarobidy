857d27e374b13a13be5bd03d4f81b7e741f1fc45
Ryan <lizkaryan626@gmail.com>
2026-02-11 12:42:39 +0300

    navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [CommonModule, RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('CenterHub');
-  isLoginPage = false;
+  showNavbar = true;
 
   constructor(private router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.isLoginPage = event.url.includes('/login');
-      }
-    });
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const hiddenRoutes = ['/login', '/register'];
+
+        const show = !hiddenRoutes.includes(this.router.url);
+
+        this.showNavbar = show;
+      });
   }
 }
