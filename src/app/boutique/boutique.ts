899f8dfc7d2f5577164b2de89c713984f5f93022
sarobidyrazafy <sarobidyraza101@gmail.com>
2026-02-28 17:12:59 +0100

    boutique compenent

---
+++ src/app/boutique/boutique.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-boutique',
+  templateUrl: './boutique.html',
+  styleUrls: ['./boutique.css'],
+  standalone: true,
+  imports: [CommonModule]
+})
+export class Boutique { }
