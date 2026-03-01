590ffab22d7cfc791437eb0971b16826c52376f4
sarobidyrazafy <sarobidyraza101@gmail.com>
2026-03-01 21:17:47 +0100

    Module pour reserver

--- src/app/boutique/reservation/reservation.ts
+++ src/app/boutique/reservation/reservation.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Room } from '../../../types/api';
-import { Observable, switchMap, take, filter } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
-import { startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-boutique-reservation',
@@ -16,17 +15,17 @@ import { startWith } from 'rxjs/operators';
   imports: [CommonModule]
 })
 export class Reservation implements OnInit {
-  rooms: Room[] = [];
+  private roomSubject = new BehaviorSubject<Room[]>([]);
+  rooms$ = this.roomSubject.asObservable();
 
-  // Propriétés pour l'alerte stylée
   alertMessage: string | null = null;
-  alertType: 'success' | 'error' = 'success';
+  alertType: 'success' | 'error' = 'error';
 
   constructor(private http: HttpClient, private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.fetchAvailableRooms().subscribe({
-      next: r => this.rooms = r,
+      next: r => this.roomSubject.next(r),
       error: err => {
         console.error('Erreur récupération salles', err);
         this.showAlert('Impossible de récupérer les salles', 'error');
@@ -40,20 +39,12 @@ export class Reservation implements OnInit {
 
   reserveRoom(room: Room): void {
     console.log('Demande de reservation :', room);
-
-    this.showAlert(`Une demande de réservation a été envoyée pour la salle ${room.name}`, 'success');
-
-    // Ici tu pourrais appeler ton backend pour enregistrer la réservation
-    // this.http.post(`${environment.baseUrl}/rooms/reserve`, { id: room.id }).subscribe(...);
-    setTimeout(() => {
-    this.router.navigate(['/boutique/reservation/select-shop']);
-  }, 1000);
+    this.router.navigate(['/boutique/reservation/select-shop'], { state: { room } });
   }
 
-  showAlert(message: string, type: 'success' | 'error' = 'success') {
+  showAlert(message: string, type: 'success' | 'error' = 'error') {
     this.alertMessage = message;
     this.alertType = type;
-
     setTimeout(() => this.alertMessage = null, 3000);
   }
 
