f93be00547856dd392b53365827ab6adb302f896
Ryan <lizkaryan626@gmail.com>
2026-03-02 14:43:56 +0300

    deployment configuration

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 // environment.ts
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:5000',
+  baseUrl: 'https://m1p13mean-ryan-sarobidy-back.onrender.com',
   tokenKey: 'center_hub_key',
   roleKey: 'center_hub_role',
   userIdKey: 'center_hub_user_id'
